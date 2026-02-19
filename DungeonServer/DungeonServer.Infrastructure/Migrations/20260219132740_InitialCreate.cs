8219ca0643c1245344dea430c42fa7636153aa5e
Matthew Lau <matthew.lau78@gmail.com>
2026-02-20 00:38:32 +1100

    add EF core generated db migrations

---
+++ DungeonServer/DungeonServer.Infrastructure/Migrations/20260219132740_InitialCreate.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DungeonServer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Width = table.Column<int>(type: "integer", nullable: false),
+                    Height = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RoomId = table.Column<int>(type: "integer", nullable: false),
+                    X = table.Column<float>(type: "real", nullable: false),
+                    Y = table.Column<float>(type: "real", nullable: false),
+                    RoomEntityId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Players_Rooms_RoomEntityId",
+                        column: x => x.RoomEntityId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomExits",
+                columns: table => new
+                {
+                    FromRoomId = table.Column<int>(type: "integer", nullable: false),
+                    ExitDirection = table.Column<int>(type: "integer", nullable: false),
+                    ToRoomId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomExits", x => new { x.FromRoomId, x.ExitDirection });
+                    table.ForeignKey(
+                        name: "FK_RoomExits_Rooms_FromRoomId",
+                        column: x => x.FromRoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_RoomEntityId",
+                table: "Players",
+                column: "RoomEntityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "RoomExits");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}
