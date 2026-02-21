bd2c428d49563f11912ac2c85724e42ce8ac1b5a
Matthew Lau <matthew.lau78@gmail.com>
2026-02-22 00:10:21 +1100

    update migrations for efplayerstore implementation

---
+++ DungeonServer/DungeonServer.Infrastructure/EntityFramework/Migrations/20260221123449_EfPlayerStoreInitial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DungeonServer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EfPlayerStoreInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOnline",
+                table: "Players",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOnline",
+                table: "Players");
+        }
+    }
+}
